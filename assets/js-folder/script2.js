74039208baf9a9dc4375d0adb7963c4455b4a2c7
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-03-10 22:23:12 -0600

    HTML file completed.

--- assets/js-folder/script2.js
+++ assets/js-folder/script2.js
@@ -1,47 +1,47 @@
-// Declared time as an element
-var timeEl = document.querySelector(".time");
-var secondsLeft = 300
-var displayQuestion = document.querySelector(".questionHere")
-var viewport = document.querySelector(".viewport")
-var msgDiv = document.querySelector("#msg");
+// // Declared time as an element
+// var timeEl = document.querySelector(".time");
+// var secondsLeft = 300
+// var displayQuestion = document.querySelector(".questionHere")
+// var viewport = document.querySelector(".viewport")
+// var msgDiv = document.querySelector("#msg");
 
 
-var questionList = ["What comes first in an HTML Document?", ]
-var answers1 = [ "A - head tag", "B - header tag", "C -h1 tag", "D -nav tag"]
+// var questionList = ["What comes first in an HTML Document?", ]
+// var answers1 = [ "A - head tag", "B - header tag", "C -h1 tag", "D -nav tag"]
 
 
 
 
-function init() {
-    for( var i = 0; i >= questionList.length; i++)
-        document.displayQuestion.textContent = questionList[i]
+// function init() {
+//     for( var i = 0; i >= questionList.length; i++)
+//         document.displayQuestion.textContent = questionList[i]
         
-        var btn1 = document.createElement("button")
-        btn1.setAttribute("id", "click1");
-        btn1.textContent = "click 1"
-        viewport.appendChild(btn1)
+//         var btn1 = document.createElement("button")
+//         btn1.setAttribute("id", "click1");
+//         btn1.textContent = "click 1"
+//         viewport.appendChild(btn1)
 
-    //     viewport.addEventListener("click", function (event) {
-    //     if (event.target.tagName.toLowerCase() === "button") {
-    //         console.log("yes it was a button")
-    // }
+//     //     viewport.addEventListener("click", function (event) {
+//     //     if (event.target.tagName.toLowerCase() === "button") {
+//     //         console.log("yes it was a button")
+//     // }
   
-    }
+//     }
 
 
 
 
-function setTime() {
-    var timeInterval = setInterval(function () {
-        secondsLeft--;
+// function setTime() {
+//     var timeInterval = setInterval(function () {
+//         secondsLeft--;
 
-        if (secondsLeft === 0) {
-            clearInterval(timeInterval)
+//         if (secondsLeft === 0) {
+//             clearInterval(timeInterval)
 
-            //need second if statement for how much time per question.
-        }
-    }, 45000 //This means that there are 45 seconds per question
-    )
-}
+//             //need second if statement for how much time per question.
+//         }
+//     }, 45000 //This means that there are 45 seconds per question
+//     )
+// }
 
-init()
+// init()
