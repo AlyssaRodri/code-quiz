53d2c790cf690b9c3a46b2c1d6a0dcfe85a02ba8
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-03-11 00:53:10 -0600

    Quiz working.

--- assets/js-folder/script2.js
+++ assets/js-folder/script2.js
@@ -1,16 +1,26 @@
 //Setting the global variables
 const startButton = document.getElementById("start-btn")
+const nextButton = document.getElementById("skip-btn")
 const questionContainerEl = document.getElementById("question-container")
 const timeEl = document.getElementById("timer-btn")
+const questionEl = document.getElementById("question")
+const answerButtonsEl = document.getElementById("answer-buttons")
 
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener("click", startGame)
-
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setQuestion()
+})
 //When start game is hit!
 function startGame(){
-    console.log(1)
+    // console.log(1)
     //hide the start button
     startButton.classList.add("hide");
+    //set shuffled questions equal to shuffled array
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     //remove the hide class from the question container so that the questions appear
     questionContainerEl.classList.remove("hide");
     //When the game is started, so is the timer!
@@ -27,22 +37,81 @@ function countDown(){
 
 //Function for the next set of questions
 function setQuestion(){
-    console.log(3)    
+    // console.log(3)
+    //add another function that will reset the box so that it looks like the same it did in the beginning instead of appending on.
+    resetState()
+    // shuffles the questions so that they are in a new order every time
+    showQuestion(shuffledQuestions[currentQuestionIndex])    
     // requires the variables for the questions
 }
 
+function showQuestion (question) {
+    //loops through our questions questions
+    questionEl.innerText = question.question
+    //this loops through our answers
+    question.answers.forEach( answer =>{
+        // creates the element for each of our buttons
+        const button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add ("btn")
+        //add if statement
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        } // create else element here for incorrect answer and how it impacts our time.
+        button.addEventListener("click", selectAnswer)
+        answerButtonsEl.appendChild(button)
+        } )
+}
+
+function resetState() {
+    nextButton.classList.add("hide");
+    // if there is a first child on the answerButtonsEl
+    while (answerButtonsEl.firstChild) {
+        //then we remove that child
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
+    }
+}
+
 //Function for selecting the answers
-function selectAnswer(){
-    console.log(4)
+function selectAnswer(e){
+    // console.log(4)
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    //create an array so we can use it in our for each shoot
+    Array.from(answerButtonsEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    //checks to make sure that we are not on the last question
+    if (shuffledQuestions.length > currentQuestionIndex + 1){
+        nextButton.classList.remove("hide")
+    } else {
+        startButton.innerText = "Restart"
+        startButton.classList.remove("hide")
+    }
     //requires the variables for the answers.
     //Will need to tie into the timer as when the answer is wrong the timer reduces
     //Will also need to store the users input into the local data
 }
 
+function setStatusClass(element, correct) {
+    clearStatusClass (element)
+    if (correct){
+        element.classList.add("correct")
+    } else{ 
+        element.classList.add("wrong")
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
+}
+
 //Create the variable for all of our questions and answers.
 //https://www.youtube.com/watch?v=riDzcEQbX6k
 
-var quizQuestions = [
+var questions = [
     {
         question: "What comes first in an HTML Document?",
         answers: [
@@ -84,7 +153,7 @@ var quizQuestions = [
         answers: [
             { text:"event.defaultPrevent()", correct: false},
             { text:"event.PreventDefault()", correct: false},
-            { text:"event.previentDefault()", correct: true},
+            { text:"event.preventDefault()", correct: true},
             { text:"console.log(event)", correct: false},
         ]
     },
