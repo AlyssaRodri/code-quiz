8b29a292c06c71bf638bea36d641c8e1117a91a0
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-03-10 23:16:58 -0600

    JavaScript2 functions created.

--- assets/js-folder/script2.js
+++ assets/js-folder/script2.js
@@ -1,10 +1,23 @@
-
-
-
+//Setting the global variables
+const startButton = document.getElementById("start-btn")
+const questionContainerEl = document.getElementById("question-container")
+const timeEl = document.getElementById("timer-btn")
+startButton.addEventListener("click", startGame)
 
 //When start game is hit!
 function startGame(){
     console.log(1)
+    //hide the start button
+    startButton.classList.add("hide");
+    //remove the hide class from the question container so that the questions appear
+    questionContainerEl.classList.remove("hide");
+    countDown();
+    setQuestion();
+}
+
+//Create a Timer function so that it can be called inside other functions
+function countDown(){
+
 }
     // 1. Timer is started
     // 2. The questions are no longer hidden
